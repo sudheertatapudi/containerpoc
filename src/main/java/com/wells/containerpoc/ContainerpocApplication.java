617271fd8b4b6d6507b453ac53dc0a8a6d5ad819
sudheertatapudi <sudheertatapudi9@gmail.com>
2025-12-08 22:05:15 +0530

    docker poc

---
+++ src/main/java/com/wells/containerpoc/ContainerpocApplication.java
@@ -0,0 +1,13 @@
+package com.wells.containerpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainerpocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContainerpocApplication.class, args);
+	}
+
+}
